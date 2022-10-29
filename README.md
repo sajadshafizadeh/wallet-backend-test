36e8c0503fb4680b82db61cc25a6556e2e220f74
Sajad Shafizadeh <vip.saj.71@gmail.com>
2022-10-29 23:25:34 +0330

    Wallet migration added & Readme file improved

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 ## Wallet Service
 
 This service provides APIs for user's wallet.
+
+
+### Installation & Getting started steps:
+
+1. Download the project
+2. Run `composer install`
+3. Make a databased named "password_manager" manually
+4. Setup the database credentials inside the `.env` file
+5. Run `php artisan migrate`
+6. Run `php artisan db:seed`
+7. Run `php artisan key:generate`
+8. Run `php artisan serve --port=8000`
+
+Now, it would be available through: http://localhost=8000
---
+++ database/migrations/2022_10_29_192308_create_wallets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWalletsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->enum('type', ['deposit', 'withdraw', 'reward', 'payment'])->comment('Sample types');
+            $table->decimal('amount', $precision = 15, $scale = 2)->comment('IRT Currency');
+            $table->decimal('tracking_code', $precision = 15, $scale = 0)->comment('Always 15 digits');
+            $table->timestamps();
+
+            // Indexe(s)
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets');
+    }
+}
