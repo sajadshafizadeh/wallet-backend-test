0e814efa1e9e1922b67fbaffdb1bae1beca553b7
Sajad Shafizadeh <vip.saj.71@gmail.com>
2022-10-30 17:39:11 +0330

    Symbolic service access token added & tests cleanup

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'service_access_token' => \Illuminate\Auth\Middleware\ServiceAccessToken::class,
     ];
 }
